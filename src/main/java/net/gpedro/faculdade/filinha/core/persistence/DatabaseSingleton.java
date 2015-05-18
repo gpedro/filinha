7208c2fe375f7cd7fca4c1525d8658c1e7af8fea
Gabriel Pedro <gpedro831@gmail.com>
2015-05-17 21:45:36 -0400

    Updating Deps and vaadin to 7.4.6

--- src/main/java/net/gpedro/faculdade/filinha/core/persistence/DatabaseSingleton.java
+++ src/main/java/net/gpedro/faculdade/filinha/core/persistence/DatabaseSingleton.java
@@ -1,7 +1,5 @@
 package net.gpedro.faculdade.filinha.core.persistence;
 
-import java.net.UnknownHostException;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
@@ -11,15 +9,11 @@ public class DatabaseSingleton {
 
     public static MongoClient getInstance() {
         if (client == null) {
-            try {
-                String mongoEnv = System.getenv("MONGO_URI");
-                if (mongoEnv != null) {
-                    client = new MongoClient(new MongoClientURI(mongoEnv));
-                } else {
-                    client = new MongoClient("localhost", 27017);
-                }
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
+            String mongoEnv = System.getenv("MONGO_URI");
+            if (mongoEnv != null) {
+                client = new MongoClient(new MongoClientURI(mongoEnv));
+            } else {
+                client = new MongoClient("localhost", 27017);
             }
         }
 
