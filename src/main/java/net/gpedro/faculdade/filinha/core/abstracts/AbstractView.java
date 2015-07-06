1212dac9b65ab950df16142f8af8acc6121f5ee2
Gabriel Pedro <gpedro831@gmail.com>
2015-07-05 23:51:48 -0400

    Add Generated Enum Field

--- src/main/java/net/gpedro/faculdade/filinha/core/abstracts/AbstractView.java
+++ src/main/java/net/gpedro/faculdade/filinha/core/abstracts/AbstractView.java
@@ -1,6 +1,9 @@
 package net.gpedro.faculdade.filinha.core.abstracts;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 
 import net.gpedro.faculdade.filinha.core.annotations.VadinhoColumn;
@@ -82,6 +85,7 @@ public abstract class AbstractView<T extends AbstractModel> extends
 
 					ComboBox cb = new ComboBox(
 							VadinhoReflect.getParsedLabel(field));
+					cb.setSizeFull();
 					cb.setItemCaption(true, vc.truth());
 					cb.setItemCaption(false, vc.falsey());
 					cb.setContainerDataSource(bc);
@@ -89,7 +93,7 @@ public abstract class AbstractView<T extends AbstractModel> extends
 					addComponent(cb);
 					continue;
 				}
-
+				
 				if (field.getType() == List.class) {
 					// System.out.println(1);
 
@@ -98,6 +102,31 @@ public abstract class AbstractView<T extends AbstractModel> extends
 					 * Table(VadinhoReflect.getParsedLabel(field), tc);
 					 * bean.bind(tbl, field.getName()); return;
 					 */
+				}
+
+				if (field.getType().isEnum()) {
+					if (field.getType().getInterfaces().length > 0) {
+						for (Class<?> interfaces: field.getType().getInterfaces()) {
+							if (interfaces == AbstractConstant.class) {
+								BeanItemContainer<AbstractConstant> bc = new BeanItemContainer<AbstractConstant>(AbstractConstant.class);
+								Method valuesMethod = field.getType().getDeclaredMethod("values", null);
+								AbstractConstant[] values = (AbstractConstant[]) valuesMethod.invoke(null, null);
+								bc.addAll(Arrays.asList(values));
+								
+								ComboBox cb = new ComboBox(
+										VadinhoReflect.getParsedLabel(field));
+								cb.setSizeFull();
+								cb.setItemCaptionPropertyId("description");
+								cb.setContainerDataSource(bc);
+								bean.bind(cb, field.getName());
+
+								addComponent(cb);
+								break;
+							}
+						}
+
+						continue;
+					}
 				}
 
 				InputText tf = new InputText(
@@ -114,7 +143,7 @@ public abstract class AbstractView<T extends AbstractModel> extends
 
 				tf.setWidth(100, Unit.PERCENTAGE);
 				addComponent(tf);
-			} catch (SecurityException | IllegalArgumentException e) {
+			} catch (SecurityException | IllegalArgumentException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
 				e.printStackTrace();
 			}
 		}
