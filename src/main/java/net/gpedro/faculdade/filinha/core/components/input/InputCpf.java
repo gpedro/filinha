c323c864dbbd8247721f52c0a63f1142f977ad8f
Gabriel Pedro <gpedro831@gmail.com>
2015-07-06 17:31:51 -0400

    Add InputCpf and Validator

---
+++ src/main/java/net/gpedro/faculdade/filinha/core/components/input/InputCpf.java
@@ -0,0 +1,27 @@
+package net.gpedro.faculdade.filinha.core.components.input;
+
+import net.gpedro.faculdade.filinha.core.validators.CpfValidator;
+
+import org.vaadin.addons.maskedtextfield.MaskedTextField;
+
+@SuppressWarnings("serial")
+public class InputCpf extends MaskedTextField {
+
+	public InputCpf() {
+		this(null, true);
+	}
+
+	public InputCpf(String caption) {
+		this(caption, true);
+	}
+	
+	public InputCpf(String caption, boolean validate) {
+		setCaption(caption);
+		setMask("###.###.###-##");
+		
+		if (validate) {
+			addValidator(new CpfValidator());
+		}
+	}
+	
+}
