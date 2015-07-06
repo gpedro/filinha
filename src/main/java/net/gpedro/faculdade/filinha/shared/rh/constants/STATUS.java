9381534fcd36523974870d87bc5aa967dcc2e173
Gabriel Pedro <gpedro831@gmail.com>
2015-07-06 17:31:29 -0400

    Removing EmReuniao Status

--- src/main/java/net/gpedro/faculdade/filinha/shared/rh/constants/STATUS.java
+++ src/main/java/net/gpedro/faculdade/filinha/shared/rh/constants/STATUS.java
@@ -4,11 +4,8 @@ import lombok.Getter;
 import net.gpedro.faculdade.filinha.core.abstracts.AbstractConstant;
 
 public enum STATUS implements AbstractConstant {
-	DISPONIVEL("Disponivel"),
-	EM_ATENDIMENTO("Em Atendimento"),
-	EM_REUNIAO("Em Reunião"),
-	AUSENTE("Ausente"),
-	INDISPONIVEL("Indisponível");
+	DISPONIVEL("Disponível"), EM_ATENDIMENTO("Em Atendimento"), AUSENTE(
+			"Ausente"), INDISPONIVEL("Indisponível");
 
 	@Getter
 	private String description;
@@ -20,19 +17,19 @@ public enum STATUS implements AbstractConstant {
 	public boolean isDisponivel() {
 		return this == STATUS.DISPONIVEL;
 	}
+
 	public boolean isEmAtendimento() {
 		return this == STATUS.EM_ATENDIMENTO;
 	}
-	public boolean isEmReuniao() {
-		return this == STATUS.EM_REUNIAO;
-	}
+
 	public boolean isAusente() {
 		return this == STATUS.AUSENTE;
 	}
+
 	public boolean isIndisponivel() {
 		return this == STATUS.INDISPONIVEL;
 	}
-	
+
 	@Override
 	public AbstractConstant findByDescription(String description) {
 		for (AbstractConstant value : values()) {
