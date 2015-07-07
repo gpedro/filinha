a089200c2cf572b984e458da898e7d197698a5b1
Gabriel Pedro <gpedro831@gmail.com>
2015-07-07 08:41:20 -0400

    Add BtnContinuar

--- src/main/java/net/gpedro/faculdade/filinha/visitante/views/AgendaView.java
+++ src/main/java/net/gpedro/faculdade/filinha/visitante/views/AgendaView.java
@@ -3,6 +3,7 @@ package net.gpedro.faculdade.filinha.visitante.views;
 import lombok.Setter;
 import net.gpedro.faculdade.filinha.core.components.button.Button;
 import net.gpedro.faculdade.filinha.core.components.input.InputCpf;
+import net.gpedro.faculdade.filinha.core.components.misc.Alert;
 import net.gpedro.faculdade.filinha.shared.courses.model.Course;
 import net.gpedro.faculdade.filinha.shared.rh.model.Coordenador;
 import net.gpedro.faculdade.filinha.visitante.ClientUI;
@@ -13,26 +14,48 @@ import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 public class AgendaView extends VerticalLayout {
-	
+
 	@Setter
 	private Coordenador coordenador;
-	
+
 	@Setter
 	private Course curso;
-	
+
+	private InputCpf inputCpf;
+
 	public AgendaView() {
 		setWidth(100, Unit.PERCENTAGE);
 		setMargin(true);
 		setSpacing(true);
 
-		InputCpf inputCpf = new InputCpf("Digite seu CPF");
+		inputCpf = new InputCpf("Digite seu CPF");
 		inputCpf.setSizeFull();
+
+		Button continuar = new Button("Continuar");
+		continuar.setSizeFull();
+		continuar.addClickListener(continuar());
 
 		Button voltar = new Button("Voltar");
 		voltar.setSizeFull();
 		voltar.addClickListener(voltar());
 
-		addComponents(inputCpf, voltar);
+		addComponents(inputCpf, continuar, voltar);
+	}
+
+	private ClickListener continuar() {
+		return new ClickListener() {
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				String cpf = inputCpf.getValue();
+				if (cpf == null) {
+					Alert.showWarn("Atenção", "Preencha o campo CPF");
+					return;
+				} else if (inputCpf.isValid()){
+					System.out.println(cpf);
+				}
+			}
+		};
 	}
 
 	private ClickListener voltar() {
