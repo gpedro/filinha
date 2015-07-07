fe63e893d4416a8b7c031e615e1ee9170ebf8f81
Gabriel Pedro <gpedro831@gmail.com>
2015-07-07 09:14:40 -0400

    Add getAlunoByCpf

--- src/main/java/net/gpedro/faculdade/filinha/shared/rh/controller/AlunoController.java
+++ src/main/java/net/gpedro/faculdade/filinha/shared/rh/controller/AlunoController.java
@@ -3,10 +3,18 @@ package net.gpedro.faculdade.filinha.shared.rh.controller;
 import net.gpedro.faculdade.filinha.core.abstracts.AbstractController;
 import net.gpedro.faculdade.filinha.shared.rh.model.Aluno;
 
+import org.mongodb.morphia.query.Query;
+
 public class AlunoController extends AbstractController<Aluno> {
 
 	public AlunoController() {
 		super(Aluno.class);
 	}
 
+	public Aluno findByCpf(String cpf) {
+		Query<Aluno> q = find();
+		q.field("cpf").equal(cpf);
+		
+		return q.get();
+	}
 }
