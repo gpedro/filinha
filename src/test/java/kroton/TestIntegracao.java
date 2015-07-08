260c4fb391784e5b5c73530b16bd141424194d3e
Gabriel Pedro <gpedro831@gmail.com>
2015-07-08 12:31:53 -0400

    Unused imports

--- src/test/java/kroton/TestIntegracao.java
+++ src/test/java/kroton/TestIntegracao.java
@@ -2,9 +2,6 @@ package kroton;
 
 import org.junit.Test;
 
-import br.com.kroton.alunoonline.integracao.KrotonLogin;
-import br.com.kroton.alunoonline.integracao.KrotonUsuario;
-
 public class TestIntegracao {
 
 	@Test
