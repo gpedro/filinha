2beb0fd77efd138957b0ee2f37f68671ac7a0a02
Gabriel Pedro <gpedro831@gmail.com>
2015-07-08 12:31:46 -0400

    Removing WebServlet

--- src/main/java/net/gpedro/faculdade/filinha/erp/ErpServlet.java
+++ src/main/java/net/gpedro/faculdade/filinha/erp/ErpServlet.java
@@ -1,11 +1,8 @@
 package net.gpedro.faculdade.filinha.erp;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 
-@WebServlet(value = { "/erp/*" }, asyncSupported = true)
 @VaadinServletConfiguration(productionMode = false, ui = ErpUI.class)
 public class ErpServlet extends VaadinServlet {
 
--- src/main/java/net/gpedro/faculdade/filinha/visitante/ClientServlet.java
+++ src/main/java/net/gpedro/faculdade/filinha/visitante/ClientServlet.java
@@ -1,11 +1,8 @@
 package net.gpedro.faculdade.filinha.visitante;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 
-@WebServlet(value = { "/client/*" }, asyncSupported = true)
 @VaadinServletConfiguration(productionMode = false, ui = ClientUI.class)
 public class ClientServlet extends VaadinServlet {
 
