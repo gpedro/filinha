37ddbc312b2396ee68e95d90ed7c6e44613738e8
Gabriel Pedro <gpedro831@gmail.com>
2015-07-08 17:51:02 -0400

    Fixin Atendimento

--- src/main/java/net/gpedro/faculdade/filinha/atendimento/AtendimentoServlet.java
+++ src/main/java/net/gpedro/faculdade/filinha/atendimento/AtendimentoServlet.java
@@ -2,13 +2,11 @@ package net.gpedro.faculdade.filinha.atendimento;
 
 import javax.servlet.annotation.WebServlet;
 
-import net.gpedro.faculdade.filinha.visitante.ClientUI;
-
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 
 @WebServlet(value = { "/atendimento/*" }, asyncSupported = true)
-@VaadinServletConfiguration(productionMode = false, ui = ClientUI.class)
+@VaadinServletConfiguration(productionMode = false, ui = AtendimentoUI.class)
 public class AtendimentoServlet extends VaadinServlet {
 
 	private static final long serialVersionUID = 3735059767375181771L;
--- src/main/java/net/gpedro/faculdade/filinha/atendimento/AtendimentoUI.java
+++ src/main/java/net/gpedro/faculdade/filinha/atendimento/AtendimentoUI.java
@@ -1,16 +1,19 @@
 package net.gpedro.faculdade.filinha.atendimento;
 
+import net.gpedro.faculdade.filinha.shared.login.view.LoginView;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
 @Theme("mytheme")
 public class AtendimentoUI extends UI {
-	
-	private static final long serialVersionUID = 1243945553082426942L;
 
-	@Override
-	protected void init(VaadinRequest request) {
-	}
+    private static final long serialVersionUID = 1243945553082426942L;
+
+    @Override
+    protected void init(VaadinRequest request) {
+	setContent(new LoginView());
+    }
 
 }
