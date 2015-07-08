c0f3ee5126c685b110512720a3a12340bce8fd95
Gabriel Pedro <gpedro831@gmail.com>
2015-07-08 10:17:18 -0400

    Add Buttons at ConfirmaView and Add SenhaPopup

--- src/main/java/net/gpedro/faculdade/filinha/visitante/views/ConfirmaView.java
+++ src/main/java/net/gpedro/faculdade/filinha/visitante/views/ConfirmaView.java
@@ -1,14 +1,22 @@
 package net.gpedro.faculdade.filinha.visitante.views;
 
 import lombok.Getter;
+import net.gpedro.faculdade.filinha.core.components.button.Button;
 import net.gpedro.faculdade.filinha.core.components.input.InputText;
+import net.gpedro.faculdade.filinha.core.components.misc.Alert;
 import net.gpedro.faculdade.filinha.core.components.misc.Head;
 import net.gpedro.faculdade.filinha.shared.courses.model.Course;
 import net.gpedro.faculdade.filinha.shared.rh.model.Aluno;
 import net.gpedro.faculdade.filinha.shared.rh.model.Coordenador;
+import net.gpedro.faculdade.filinha.visitante.ClientUI;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.Page;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
@@ -60,13 +68,71 @@ public class ConfirmaView extends VerticalLayout implements View {
 
         // ----------
 
+        Button continuar, retornar, cancelar;
+        
+        continuar = new Button("Emitir Senha");
+        continuar.setSizeFull();
+        continuar.addClickListener(continuar());
+        
+        retornar  = new Button("Retornar");
+        retornar.setSizeFull();
+        retornar.addClickListener(retornar());
+        
+        cancelar  = new Button("Cancelar");
+        cancelar.setSizeFull();
+        cancelar.addClickListener(cancelar());
+        
+        GridLayout botoes = new GridLayout(3, 1);
+        botoes.setSpacing(true);
+        botoes.setSizeFull();
+        botoes.addComponents(retornar, cancelar, continuar);
+        
+        // ----------
+        
         addComponent(Head.H3.toComponent("Dados do Solicitante"));
 		addComponents(cpf, nome, telefone);
         addComponent(Head.H3.toComponent("Dados do Solicitado"));
 		addComponents(professor, materia);
 		addComponent(Head.HR.toComponent(null));
+		addComponent(botoes);
 	}
 
+	private Button.ClickListener continuar() {
+		return new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				SenhaPopup p = new SenhaPopup("001");
+				if (!p.isAttached()) {
+					ClientUI.getCurrent().addWindow(p);
+				}
+			}
+		};
+	}
+	
+	private Button.ClickListener retornar() {
+		return new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				AgendaView ag = new AgendaView();
+				ag.setCurso(curso);
+				ag.setCoordenador(coordenador);
+				ClientUI.getCurrent().setContent(ag);
+			}
+		};
+	}
+	
+	private ClickListener cancelar() {
+		return new ClickListener() {
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				ClientUI.getCurrent().setContent(new SelectCourse());
+			}
+		};
+	}
+	
 	@Override
 	public void enter(ViewChangeEvent event) {
 	}
---
+++ src/main/java/net/gpedro/faculdade/filinha/visitante/views/SenhaPopup.java
@@ -0,0 +1,64 @@
+package net.gpedro.faculdade.filinha.visitante.views;
+
+
+import net.gpedro.faculdade.filinha.core.components.button.Button;
+import net.gpedro.faculdade.filinha.core.components.misc.Head;
+import net.gpedro.faculdade.filinha.visitante.ClientUI;
+
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+import elemental.events.KeyboardEvent.KeyCode;
+
+@SuppressWarnings("serial")
+public class SenhaPopup extends Window {
+
+	public SenhaPopup(String senha) {
+
+		setCaption("Senha Gerada");
+		setModal(true);
+		setResizable(false);
+		setClosable(true);
+		setDraggable(false);
+		
+		setCloseShortcut(KeyCode.ESC);
+		setCloseShortcut(KeyCode.ENTER);
+		addCloseListener(new CloseListener() {
+			
+			@Override
+			public void windowClose(CloseEvent e) {
+				ClientUI.getCurrent().setContent(new SelectCourse());
+			}
+		});
+
+		VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		layout.setSpacing(true);
+		
+		Label titulo = Head.H3.toComponent("Senha gerada com sucesso!");
+		Label pass = new Label("<div style=\"font-size:2em;text-align:center;margin-bottom:30px;\"><strong>" + senha + "</strong></div>", ContentMode.HTML);
+		pass.addStyleName("huehue");
+		
+		Button fechar = new Button("Fechar");
+		fechar.setSizeFull();
+		fechar.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				SenhaPopup.this.close();
+			}
+		});
+		
+		layout.addComponents(titulo, pass, fechar);
+		layout.setComponentAlignment(titulo, Alignment.MIDDLE_CENTER);
+		layout.setComponentAlignment(pass, Alignment.MIDDLE_CENTER);
+		
+		setContent(layout);
+		
+	}
+}
