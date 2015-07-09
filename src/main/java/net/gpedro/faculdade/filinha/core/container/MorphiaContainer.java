7d1112b97544883b24a4bdc9bfd48207c4cf2aaf
Gabriel Pedro <gpedro831@gmail.com>
2015-07-09 11:59:50 -0400

    Fixing TotalPages on Morphia Container

--- src/main/java/net/gpedro/faculdade/filinha/core/container/MorphiaContainer.java
+++ src/main/java/net/gpedro/faculdade/filinha/core/container/MorphiaContainer.java
@@ -18,117 +18,117 @@ import com.vaadin.ui.Label;
 
 @SuppressWarnings("serial")
 public class MorphiaContainer<T extends AbstractModel> extends
-		BeanContainer<ObjectId, T> implements Container.Ordered,
-		Container.Sortable {
+	BeanContainer<ObjectId, T> implements Container.Ordered,
+	Container.Sortable {
 
-	@Setter
-	private AbstractController<T> controller;
-	public Query<T> query;
-	private Query<T> queryOriginal;
+    @Setter
+    private AbstractController<T> controller;
+    public Query<T> query;
+    private Query<T> queryOriginal;
 
-	@Setter
-	@Getter
-	public int rowsPerPage = 2;
+    @Setter
+    @Getter
+    public int rowsPerPage = 2;
 
-	@Getter
-	public int currentPage = 1;
+    @Getter
+    public int currentPage = 1;
 
-	@Getter
-	public int totalPages;
+    @Getter
+    public int totalPages;
 
-	@Getter
-	public int totalRows = -1;
+    @Getter
+    public int totalRows = -1;
 
-	@Setter
-	public Label label;
+    @Setter
+    public Label label;
 
-	private int offset = 0;
+    private int offset = 0;
 
-	public MorphiaContainer(Class<T> objClass) {
-		super(objClass);
+    public MorphiaContainer(Class<T> objClass) {
+	super(objClass);
 
-		this.setBeanIdResolver(new BeanIdResolver<ObjectId, T>() {
+	this.setBeanIdResolver(new BeanIdResolver<ObjectId, T>() {
 
-			@Override
-			public ObjectId getIdForBean(T bean) {
-				return bean.getId();
-			}
-		});
+	    @Override
+	    public ObjectId getIdForBean(T bean) {
+		return bean.getId();
+	    }
+	});
+    }
+
+    public void setQuery(Query<T> q) {
+	this.query = this.queryOriginal = q;
+    }
+
+    public void refreshPagination() {
+
+	if (label == null) {
+	    label = new Label();
 	}
 
-	public void setQuery(Query<T> q) {
-		this.query = this.queryOriginal = q;
+	label.setValue("Página " + getCurrentPage() + " de " + getTotalPages());
+
+    }
+
+    public void build() {
+	if (query == null) {
+	    setQuery(controller.find());
 	}
 
-	public void refreshPagination() {
-
-		if (label == null) {
-			label = new Label();
-		}
-
-		label.setValue("Página " + getCurrentPage() + " de "
-				+ ((getTotalPages() == 0) ? 1 : getTotalPages()));
-
+	if (totalRows == -1) {
+	    totalRows = (queryOriginal.asList().size());
 	}
 
-	public void build() {
-		if (query == null) {
-			setQuery(controller.find());
-		}
-
-		if (totalRows == -1) {
-			totalRows = queryOriginal.asList().size();
-		}
-
-		totalPages = (int) Math.ceil(totalRows / rowsPerPage);
-		if (totalPages > 1) {
-			totalPages++;
-		}
-
-		offset = rowsPerPage * (currentPage - 1);
-
-		query.limit(rowsPerPage).offset(offset);
-		removeAllItems();
-		this.addAll(query.asList());
-		refreshPagination();
+	totalPages = (int) Math.ceil(totalRows / rowsPerPage);
+	totalPages = (totalPages == 0) ? 1 : totalPages;
+	if (totalPages > 1) {
+	    totalPages++;
 	}
 
-	@Override
-	protected void sortContainer(Object[] propertyId, boolean[] ascending) {
-		int index = 0;
-		List<String> order = new ArrayList<String>();
-		for (Object property : propertyId) {
-			order.add(((!ascending[index]) ? "-" : "") + property);
-			index++;
-		}
+	offset = rowsPerPage * (currentPage - 1);
 
-		query.order(StringUtils.join(order, ","));
-		removeAllItems();
-		goToPage(currentPage);
+	query.limit(rowsPerPage).offset(offset);
+	removeAllItems();
+	this.addAll(query.asList());
+	refreshPagination();
+    }
+
+    @Override
+    protected void sortContainer(Object[] propertyId, boolean[] ascending) {
+	int index = 0;
+	List<String> order = new ArrayList<String>();
+	for (Object property : propertyId) {
+	    order.add(((!ascending[index]) ? "-" : "") + property);
+	    index++;
 	}
 
-	public void goToPage(int page) {
-		currentPage = page;
+	query.order(StringUtils.join(order, ","));
+	removeAllItems();
+	goToPage(currentPage);
+    }
 
-		build();
+    public void goToPage(int page) {
+	currentPage = page;
+
+	build();
+    }
+
+    public boolean nextPage() {
+	int nextPage = currentPage + 1;
+	if (nextPage <= totalPages) {
+	    goToPage(nextPage);
+	    return true;
+	}
+	return false;
+    }
+
+    public boolean previousPage() {
+	int previousPage = currentPage - 1;
+	if (previousPage > 0) {
+	    goToPage(previousPage);
+	    return true;
 	}
 
-	public boolean nextPage() {
-		int nextPage = currentPage + 1;
-		if (nextPage <= totalPages) {
-			goToPage(nextPage);
-			return true;
-		}
-		return false;
-	}
-
-	public boolean previousPage() {
-		int previousPage = currentPage - 1;
-		if (previousPage > 0) {
-			goToPage(previousPage);
-			return true;
-		}
-
-		return false;
-	}
+	return false;
+    }
 }
